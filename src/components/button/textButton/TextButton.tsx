1bdd5652c79fc0386bf3a9dcce368c2b5b135cb5
resister-boy <hassanpumped17@gmail.com>
2024-06-06 14:19:23 +0900

    feat: add base button and text button

---
+++ src/components/button/textButton/TextButton.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from '@/components/button/textButton/TextButton.module.scss'
+import classNames from 'classnames/bind'
+
+const cn = classNames.bind(styles)
+
+const TextButton = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TextButton
