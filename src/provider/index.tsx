26177075b571f017014845caa490d8b25821258f
resister-boy <hassanpumped17@gmail.com>
2024-06-06 14:16:10 +0900

    feat: add theme settings

--- src/provider/index.tsx
+++ src/provider/index.tsx
@@ -1,11 +1,16 @@
 'use client'
-import React, { Fragment, ReactNode } from 'react'
+import React, { Fragment, ReactNode, useEffect } from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Layout from '@/layout';
 
 const queryClient = new QueryClient();
 
 const BaseProvider = ({ children }: { children: ReactNode }) => {
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', 'dark');
+  }, []);
+  
   return (
     <Fragment>
       <QueryClientProvider client={queryClient}>
