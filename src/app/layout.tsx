3ab5cb8e6c233676a383573cbd73400b59b39239
resister-boy <hassanpumped17@gmail.com>
2024-06-06 18:09:25 +0900

    feat: add create portal root element

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({
           <ReduxProvider>
             <BaseProvider>
               {children}
+              <div id="modal-root" />
             </BaseProvider>
           </ReduxProvider>
         </NextAuthProvider>
