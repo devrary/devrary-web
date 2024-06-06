b8eb01a40bd611b43f921db51ed7899ce3520a72
resister-boy <hassanpumped17@gmail.com>
2024-06-06 14:16:46 +0900

    feat: add base login screens

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import styles from '@/app/(auth)/login/page.module.scss'
 import classNames from 'classnames/bind'
+import LoginClientPage from '@/app/(auth)/login/LoginClientPage'
 
 const cn = classNames.bind(styles)
 
 const LoginPage = () => {
   return (
-    <main></main>
+    <main>
+      <LoginClientPage />
+    </main>
   )
 }
 
