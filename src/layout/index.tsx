1c96957cf347114fed0b8b6b01fd4ecb9e8a1a1c
resister-boy <hassanpumped17@gmail.com>
2024-06-06 14:18:55 +0900

    feat: add conditional show layout

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,14 +1,28 @@
 'use client'
-import React, { Fragment, ReactNode } from 'react'
+import React, { Fragment, ReactNode, useMemo } from 'react'
 import Header from '@/layout/header/Header'
 import Footer from '@/layout/footer/Footer'
+import { usePathname } from 'next/navigation'
 
 const Layout = ({ children }: { children: ReactNode }) => {
+  const pathname = usePathname();
+
+  const showHeader = useMemo(() => {
+    if (pathname === '/login') return false;
+    return true;
+  }, [pathname])
+
+
+  const showFooter = useMemo(() => {
+    if (pathname === '/login') return false;
+    return true;
+  }, [pathname])
+
   return (
     <Fragment>
-      <Header />
+      {showHeader && <Header />}
       {children}
-      <Footer />
+      {showFooter && <Footer />}
     </Fragment>
   )
 }
