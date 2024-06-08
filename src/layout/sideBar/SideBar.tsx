6e45797d6f055466f67f638dcddf82c03b53c772
resister-boy <hassanpumped17@gmail.com>
2024-06-08 17:01:10 +0900

    feat: add base layout

--- src/layout/sideBar/SideBar.tsx
+++ src/layout/sideBar/SideBar.tsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useCallback, useMemo } from 'react';
+import styles from '@/layout/sideBar/SideBar.module.scss';
+import classNames from 'classnames/bind';
+import NotificationContainer from '@/widget/sideContent/notification/NotificationContainer';
+import SearchContainer from '@/widget/sideContent/search/SearchContainer';
+import { useSelector } from 'react-redux';
+import { getSideState } from '@/state/slice/sideSlice';
+
+const cn = classNames.bind(styles);
 
 const SideBar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const { name, visibility } = useSelector(getSideState);
 
-export default SideBar
+  const state = useMemo(() => {
+    if (!name || !visibility) return 'disappear';
+    return 'appear';
+  }, [name, visibility]);
+
+  const renderSideBar = useCallback(() => {
+    if (name === 'notification') {
+      return <NotificationContainer />;
+    } else if (name === 'search') {
+      return <SearchContainer />;
+    } else {
+      return null;
+    }
+  }, [name]);
+  return <aside className={cn(`container-${state}`)}>{renderSideBar()}</aside>;
+};
+
+export default SideBar;
