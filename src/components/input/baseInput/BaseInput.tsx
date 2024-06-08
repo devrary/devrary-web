59dbd93bcc8841bd5c4810cd3209fd50d3221e5b
resister-boy <hassanpumped17@gmail.com>
2024-06-08 17:00:38 +0900

    feat: add base components

--- src/components/input/baseInput/BaseInput.tsx
+++ src/components/input/baseInput/BaseInput.tsx
@@ -1,6 +1,13 @@
-import React, { ChangeEvent, ReactNode } from 'react'
-import styles from '@/components/input/baseInput/BaseInput.module.scss'
-import classNames from 'classnames/bind'
+import React, {
+  ChangeEvent,
+  ReactNode,
+  useCallback,
+  useMemo,
+  useState,
+} from 'react';
+import styles from '@/components/input/baseInput/BaseInput.module.scss';
+import classNames from 'classnames/bind';
+import { IoEyeOffOutline, IoEyeOutline } from 'react-icons/io5';
 
 type Props = {
   id: string;
@@ -10,19 +17,19 @@ type Props = {
   placeholder: string;
   readonly?: boolean;
   disabled?: boolean;
-  value: string | (number | ''),
-  error?: any | null,
-  children?: ReactNode,
-  classNames?: string,
-  onChange: (event: ChangeEvent<HTMLInputElement>) => void,
-}
+  value: string | (number | '');
+  error?: any | null;
+  children?: ReactNode;
+  classNames?: string;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+};
 
-const cn = classNames.bind(styles)
+const cn = classNames.bind(styles);
 
 const BaseInput = ({
   id,
   type,
-  label = "",
+  label = '',
   labelVisible = false,
   placeholder,
   readonly = false,
@@ -31,15 +38,58 @@ const BaseInput = ({
   error = null,
   classNames = '',
   onChange,
-  children
+  children,
 }: Props) => {
+  const isPassword = type === 'password';
+  const [_type, setType] = useState<'text' | 'password' | 'number' | 'email'>(
+    type,
+  );
+
+  const handlePwView = useCallback(() => {
+    if (_type === 'text') {
+      setType('password');
+    } else {
+      setType('text');
+    }
+  }, [_type]);
+
+  const passwordIcon = useMemo(() => {
+    return _type === 'password' ? (
+      <IoEyeOffOutline color="#CCCCCC" size={18} />
+    ) : (
+      <IoEyeOutline color="#CCCCCC" size={18} />
+    );
+  }, [_type]);
+
   return (
     <div className={cn('container')}>
-      <label htmlFor={id} className={cn('label')}>{label}</label>
-      <input type={type} placeholder={placeholder} className={cn('input')} disabled={disabled} readOnly={readonly} onChange={onChange} value={value} />
+      <label htmlFor={id} className={cn('label')}>
+        {label}
+      </label>
+      <input
+        type={_type}
+        placeholder={placeholder}
+        className={cn('input')}
+        disabled={disabled}
+        readOnly={readonly}
+        onChange={onChange}
+        value={value}
+      />
       {children && <div className={cn('icon')}>{children}</div>}
+      {isPassword && (
+        <button
+          className={cn('password')}
+          onClick={(event) => {
+            handlePwView();
+            event.preventDefault();
+            event.stopPropagation();
+          }}
+        >
+          {passwordIcon}
+        </button>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default BaseInput
+export default BaseInput;
